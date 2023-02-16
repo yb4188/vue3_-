8a7a2505792e5525bd1828b51b7355473a200f73
yb4188 <2900856508@qq.com>
2023-02-16 12:51:44 +0800

    初始化项目

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import {
+  createRouter,
+  createWebHashHistory
+} from 'vue-router'
+
+const routes = [
+
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
