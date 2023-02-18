7972a19a2cf08d6c1d5f2d32af5d0e4e763ec59d
yb4188 <2900856508@qq.com>
2023-02-18 13:59:38 +0800

    完成了头部标题的数据渲染

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -12,12 +12,19 @@
 import AppTopNav from '../components/app-topNav.vue'
 import AppHeader from '../components/app-header.vue'
 import AppFooter from '../components/app-footer.vue'
+import { useStore } from 'vuex';
+
 export default {
   name: "xtx-Layout",
   components: {
     AppTopNav,
     AppHeader,
     AppFooter
+  },
+  setup(){
+    const store = useStore()
+    store.dispatch('category/getList')
+    return
   }
 };
 </script>
